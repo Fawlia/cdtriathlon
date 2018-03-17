2fe9b86efa13067752a7ad5c9a2eb942b8d7d6f7
Yohann <yohann.t@codeur.online>
2018-03-17 09:29:03 +0100

    [prod]Page des single-post terminée

--- tria/footer.php
+++ tria/footer.php
@@ -1,8 +1,12 @@
-<footer>
-	<p>Thème créer par Mourad, Sandrine, Yohann et Romain</p>
+<footer class="container-fluid">
+	<div class="row">
+		<div id="foot" class="col-12 d-flex justify-content-center align-items-center">
+			<p>Thème créé par Mourad, Sandrine, Yohann et Romain | ACS Vesoul 2018</p>
+		</div>
+	</div>
 </footer>
 
-	<?php wp_footer(); ?>
+<?php wp_footer(); ?>
 </body>
-</html>
 
+</html>
--- tria/single-post.php
+++ tria/single-post.php
@@ -1,7 +1,7 @@
 <?php get_header(); ?>
 
 <main>
-	<div id="single-team" class="d-flex flex-column align-content-between justify-content-between">
+	<div id="single-post" class="d-flex flex-column align-content-between justify-content-between">
 
 
 
@@ -9,30 +9,39 @@
 			<div class="container">
 				<div class="row">
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-						<img src="<?php the_post_thumbnail_url(); ?>" alt="Image d'un article" class="img-fluid" id="img_principale" width="550" height="auto">
+					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center" id="img_principale" style="background-image: url(<?php the_post_thumbnail_url(); ?>);">
+						<!--<img src="" alt="Image d'un article" class="img-fluid" id="img_principale" width="550" height="auto">-->
+						<div id="jaune" class="d-flex justify-content-center">
+							<h1>
+								<?php the_title(); ?>
+							</h1>
+						</div>
+
 					</div>
 					<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center">
-
-						<h1>
-							<?php the_title(); ?>
-						</h1>
 						<div id="post_cont">
 							<?php the_content(); ?>
 						</div>
-						
-							<p class="small text-left">
-								<span class="text-left">Ecrit par <?php echo get_the_author(); ?> le <?php echo get_the_date(); ?>
-							</p>
-					
-						
+					</div>
 
-						<?php endwhile; ?>
-						<?php endif; ?>
+					<div class="col-auto text-right">
+						<p id="info" class="small text-left">
+							Ecrit par
+							<?php echo get_the_author(); ?> le
+							<?php echo get_the_date(); ?>
+
+						</p>
 					</div>
-					<div class="col-12">
-						<button class="btn btn-default hidden"><a href="http://localhost/wordpress/#firstPage/slide2"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> RETOUR</a></button>
+					<div class="col-12 text-right">
+						<button class="btn btn-default hidden"><a href="http://localhost/wordpress/#firstPage"><i class="fa fa-long-arrow-left" aria-hidden="true"></i> RETOUR</a></button>
 					</div>
+
+
+
+					<?php endwhile; ?>
+					<?php endif; ?>
+
+
 				</div>
 			</div>
 		</section>
